1dc7cc6e418618b794e6e6ca809ef06553e8bdfc
rocco1967 <gianfranco.fa@gmail.com>
2023-01-16 11:39:48 +0100

    Update API.py

--- API.py
+++ API.py
@@ -1,5 +1,3 @@
-
-"""
 import openai
 import pandas as pd
 import streamlit as st
@@ -8,9 +6,18 @@ import numpy as np
 from PIL import Image
 st.header('.....................My name is Ambrose..................')
 st.subheader('.............................I^m here to serve you...........................')
-
+st.subheader('I am an advanced neural network specialized in responding in natural language')
+image=Image.open('concierge.JPG')#('sfera.JPG')
+image = image.resize((1000, 400))
+st.image(image)
 st.subheader('To keep the service active make a small donation with PayPal.. Thank you')
 st.success('gianfranco.fa@gmail.com')
+#openai.api_key = os.getenv("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")
+#os.environ["OPENAI_API_KEY"] = "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"
+#openai.api_key = os.environ["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"]
+#openai.api_key=st.secrets['OPEN_APY_KEY']
+#openai.api_key = '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'#
+#model_engine ="text-davinci-003"#text-curie-001"# "text-davinci-003"
 
 st.subheader('For use and explanation models read this:')
 st.success('https://beta.openai.com/docs/models/overview')
@@ -33,16 +40,16 @@ openai.api_key=st.secrets['OPEN_APY_KEY']
 
     
 prompt =st.text_area('YOUR REQUEST:')
-
+#if prompt is not None:
 completions = openai.Completion.create(engine=model_engine,prompt=prompt,max_tokens=1024,n=1, stop=None,
                                       frequency_penalty=0.0,temperature=0.0,)#top_p=1
-
+#message =(completions.choices[0].text)#
+#st.write(message)
 if st.button('RUN'):
     message =(completions.choices[0].text)#
     st.write(message)
-
+#else:
+    #st.write('inserisci la richiesta')
+#text_contents = (message)
 st.download_button('download the result on your PC.. After first sloping Run....',(completions.choices[0].text))
-
-
-
 

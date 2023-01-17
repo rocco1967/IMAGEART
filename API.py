da409ea4e3e3d541b665e3b64807388c304b4dc6
rocco1967 <gianfranco.fa@gmail.com>
2023-01-17 17:21:37 +0100

    Update API.py

--- API.py
+++ API.py
@@ -39,7 +39,7 @@ st.success('https://beta.openai.com/docs/models/overview')
 #st.download_button('download the result on your PC.. After first sloping Run....',(completions.choices[0].text))
 ###########################################################################################################################
 #image=st.file_uploader("Choose a file_csv")
-image2= Image.open(st.file_uploader("Choose a file_csv")
+image2= Image.open(st.file_uploader("Choose a file_csv"))
 #prompt=st.image(image,output_format='PNG')
 
 openai.api_key=st.secrets['OPEN_APY_KEY']
